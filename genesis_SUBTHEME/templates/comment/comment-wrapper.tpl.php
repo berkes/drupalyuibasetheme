7904642af7ab60f735bb94bde813b0f1c7aeb117
jmburnz <jmburnz>
2009-04-19 17:33:14 +0000

    new tpl structure

---
+++ genesis_SUBTHEME/templates/comment/comment-wrapper.tpl.php
@@ -0,0 +1,16 @@
+<?php
+// $Id: comment-wrapper.tpl.php,v 1.1.2.1 2009/04/19 17:33:14 jmburnz Exp $
+
+/**
+ * @file comment-wrapper.tpl.php
+ * Default theme implementation to wrap comments.
+ *
+ * @see template_preprocess_comment_wrapper()
+ * @see theme_comment_wrapper()
+ */
+?>
+<?php if ($content): ?>
+  <div id="comment-wrapper">
+    <?php print $content; ?>
+  </div>
+<?php endif; ?> <!-- /silence coder -->
