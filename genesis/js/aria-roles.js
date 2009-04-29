48312cea6281cc11e17eade16118738c68241cb6
jmburnz <jmburnz>
2009-04-29 21:38:46 +0000

    add node links and pager as role navigation

--- genesis/js/aria-roles.js
+++ genesis/js/aria-roles.js
@@ -1,4 +1,4 @@
-// $Id: aria-roles.js,v 1.1.2.1 2009/04/29 20:51:12 jmburnz Exp $
+// $Id: aria-roles.js,v 1.1.2.2 2009/04/29 21:38:46 jmburnz Exp $
 
 /**
  * Insert WAI-ARIA Landmark Roles (Roles for Accessible Rich Internet Applications)
@@ -17,7 +17,7 @@ if (Drupal.jsEnabled) {
     $("#main-content").attr("role","main");
 		  $(".block, .sidebar, .region").attr("role","complementary");
     $("#search, #block-search-0").attr("role","search");
-    $("#nav, #breadcrumb, .block-menu, #block-user-1, .block-book, .block-forum, .block-blog, .block-statistics-0, .block-aggregator").attr("role","navigation");
+    $("#nav, #breadcrumb, .block-menu, #block-user-1, .block-book, .block-forum, .block-blog, .block-statistics-0, .block-aggregator, .node ul.links, ul.pager").attr("role","navigation");
 		  $("#footer-message").attr("role","contentinfo");
     $(".node").attr("role","article");
 		  $(".block-system").removeAttr("role","complementary");
