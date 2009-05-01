83cd8831efac7898141c47b856396ab9b4baf829
jmburnz <jmburnz>
2009-05-01 18:04:45 +0000

    minified by default

--- genesis/js/script.js
+++ genesis/js/script.js
@@ -1,34 +1,6 @@
-// $Id: script.js,v 1.1.2.2 2009/04/29 20:51:12 jmburnz Exp $
+// $Id: script.js,v 1.1.2.3 2009/05/01 18:04:45 jmburnz Exp $
 
 /**
  * Animates submit buttons, prevents clicking twice.
  */
-var Genesis = {};
-// jump to the value in a select drop down
-Genesis.go = function(e) {
-  var destination = e.options[e.selectedIndex].value;
-  if (destination && destination != 0) location.href = destination;
-};
-// prevent users from clicking a submit button twice
-Genesis.formCheck = function() {
-  // only apply this to node and comment and new user registration forms
-  var forms = $("#node-form>div>div>#edit-submit,#comment-form>div>#edit-submit,#user-register>div>#edit-submit");
-  // insert the saving div now to cache it for better performance and to show the loading image
-  $('<div id="saving"><p class="saving">Saving&hellip;</p></div>').insertAfter(forms);
-  forms.click(function() {
-    $(this).siblings("input[@type=submit]").hide();
-    $(this).hide();
-    $("#saving").show();
-    var notice = function() {
-      $('<p id="saving-notice">Not saving? Wait a few seconds, reload this page, and try again.</p>').appendTo("#saving").fadeIn();
-    };
-    // append notice if form saving isn't work, perhaps a timeout issue
-    setTimeout(notice, 24000);
-  });
-};
-// Global Killswitch.
-if (Drupal.jsEnabled) {
-  $(document).ready(Genesis.formCheck);
-}
-
-
+var Genesis={};Genesis.go=function(e){var destination=e.options[e.selectedIndex].value;if(destination&&destination!=0)location.href=destination;};Genesis.formCheck=function(){var forms=$("#node-form>div>div>#edit-submit,#comment-form>div>#edit-submit,#user-register>div>#edit-submit");$('<div id="saving"><p class="saving">Saving…</p></div>').insertAfter(forms);forms.click(function(){$(this).siblings("input[@type=submit]").hide();$(this).hide();$("#saving").show();var notice=function(){$('<p id="saving-notice">Not saving? Wait a few seconds, reload this page, and try again.</p>').appendTo("#saving").fadeIn();};setTimeout(notice,24000);});};if(Drupal.jsEnabled){$(document).ready(Genesis.formCheck);}
