af52071c7e379fd6949031e038c96d29ff314176
jmburnz <jmburnz>
2009-05-03 22:25:03 +0000

    add new doc block notes

--- genesis_LITE/templates/page/page.tpl.php
+++ genesis_LITE/templates/page/page.tpl.php
@@ -1,12 +1,85 @@
 <?php
-// $Id: page.tpl.php,v 1.1.2.9 2009/05/02 13:59:51 jmburnz Exp $
+// $Id: page.tpl.php,v 1.1.2.10 2009/05/03 22:25:17 jmburnz Exp $
 
 /**
  * @file page.tpl.php
  * Theme implementation to display a single Drupal page.
  *
- * @see template_preprocess()
- * @see template_preprocess_page()
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *     least, this will always default to /.
+ * - $css: An array of CSS files for the current page.
+ * - $directory: The directory the theme is located in, e.g. themes/garland or
+ *     themes/garland/minelli.
+ * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Page metadata:
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *     so on).
+ * - $head_title: A modified version of the page title, for use in the TITLE tag.
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *     for the page.
+	* - $section_class: A CSS class that uses .section + the 1st URL argument, allows for
+	*     themeing site sections based on path.
+ * - $classes: A set of CSS classes (preprocess $body_classes + Genesis custom classes). 
+	*     This contains flags indicating the current layout (multiple columns, single column), 
+	*     the current path, whether the user is logged in, and so on.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *     when linking to the front page. This includes the language domain or prefix.
+ * - $site_logo: The preprocessed $logo varaible. Includes the path to the logo image, 
+	*     as defined in theme configuration and wrapped in an anchor linking to the homepage.
+ * - $site_name: The name of the site (preprocessed) wrapped in an anchor linking to the homepage. 
+	*     Empty when display has been disabled in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *     in theme settings.
+ * - $mission: The text of the site mission, empty when display has been disabled
+ *     in theme settings.
+ *
+ * Navigation:
+ * - $primary_menu: The preprocessed $primary_links (array), an array containing primary 
+	*     navigation links for the site, if they have been configured.
+ * - $secondary_menu: The preprocessed $secondary_links (array), an array containing secondary 
+	*     navigation links for the site, if they have been configured.
+ * - $search_box: HTML to display the search box, empty if search has been disabled.
+ *
+ * Page content (in order of occurrance in the default page.tpl.php):
+	* - $leaderboard: Custom region for displaying content at the top of the page, useful
+	*     for displaying a banner.
+	* - $header: The header blocks region for display content in the header.
+	* - $secondary_content: Full width custom region for displaying content between the header
+	*     and the main content columns.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+	* - $content_top: A custom region for displaying content above the main content.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $help: Dynamic help text, mostly for admin pages.
+ * - $messages: HTML for status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the view
+ *     and edit tabs when displaying a node).
+ * - $content: The main content of the current Drupal page.
+	* - $content_bottom: A custom region for displaying content above the main content.
+ * - $left: Region for the left sidebar.
+ * - $right: Region for the right sidebar.
+ * - $tertiary_content: Full width custom region for displaying content between main content 
+	*   columns and the footer.
+	*
+ * Footer/closing data:
+ * - $footer : The footer region.
+ * - $footer_message: The footer message as defined in the admin settings.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $closure: Final closing markup from any modules that have altered the page.
+ *     This variable should always be output last, after all other dynamic content.
+ *
+ * @see genesis_preprocess_page()
  */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
